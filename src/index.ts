c2f123ce1ab1fac9cacb221fd4e5265880a3357c
TheNightmareX <TheNightmareX@foxmail.com>
2021-03-22 06:52:18 +0800

    feat(index): add exports

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ export {
   DateField,
   Field,
   FieldOptions,
+  FieldValues,
   ListField,
   NumberField,
   StringField,
-  FieldValues,
 } from "./fields";
 export { build } from "./resources";
-export { transformCase, mixinStatic } from "./utils";
+export { AbstractStorage, PK, Storage } from "./storage";
+export { ExtractKeys, mixinStatic, transformCase, Values } from "./utils";
 export { ValidationError } from "./validators";
